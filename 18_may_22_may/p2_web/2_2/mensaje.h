207b26dc484c334cffaf16cdba66b198ea0d9857
jmanjma <45376226+jmanjma@users.noreply.github.com>
2020-05-24 16:51:55 -0500

    Add files via upload

---
+++ 18_may_22_may/p2_web/2_2/mensaje.h
@@ -0,0 +1,17 @@
+#ifndef MENSAJE_H_
+#define MENSAJE_H_
+
+#define TAM_MAX_DATA 4000
+#define NUM_SERVS 2
+
+// Definicion del formato de mensaje
+struct mensaje {
+    int messageType;            // 0=Solicitud, 1=Respuesta
+    unsigned int requestId;     // Identificador del mensaje
+    int operationId;            // Identificador de la operacion
+    char arguments[TAM_MAX_DATA];
+    int lamport[NUM_SERVS];
+    char id_serv;
+};
+
+#endif
